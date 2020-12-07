5e585a4dd35f1c827fdd6f9b26e85584a0bae5be
Samatar Xasan <Samatar.Xasan@gmail.com>
2020-12-07 10:39:44 +0000

    Initial Upload for 6th Project

---
+++ script.js
@@ -0,0 +1,22 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+checkBoxes()
+function checkBoxes() {
+    //Finds the window height which is then reduced slighly to prevent the last box from peaking
+   const triggerBottom = window.innerHeight / 5 * 4
+
+   boxes.forEach(box => {
+    //    Finds the distance of the rectangle to the bottom of the viewport
+       const boxTop = box.getBoundingClientRect().top;
+
+       if (boxTop < triggerBottom) {
+           box.classList.add('show')
+       } else {
+           box.classList.remove('show')
+       }
+   })
+}
+
+
+//animate on scroll library or Jquery as an alternative to this
