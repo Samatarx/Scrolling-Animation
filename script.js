e4581daf6fd4c8884375738079ccccefac156168
Samatar Xasan <Samatar.Xasan@gmail.com>
2020-12-07 11:42:30 +0000

    Modifications to footer

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const boxes = document.querySelectorAll('.box')
+const boxes = document.querySelectorAll(".box");
+const foot = document.querySelector('footer')
 
-window.addEventListener('scroll', checkBoxes)
-checkBoxes()
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
 function checkBoxes() {
-    //Finds the window height which is then reduced slighly to prevent the last box from peaking
-   const triggerBottom = window.innerHeight / 5 * 4
+  //Finds the window height which is then reduced slighly to prevent the last box from peaking
+  const triggerBottom = (window.innerHeight / 5) * 4;
 
-   boxes.forEach(box => {
+  boxes.forEach((box) => {
     //    Finds the distance of the rectangle to the bottom of the viewport
-       const boxTop = box.getBoundingClientRect().top;
+    const boxTop = box.getBoundingClientRect().top;
 
-       if (boxTop < triggerBottom) {
-           box.classList.add('show')
-       } else {
-           box.classList.remove('show')
-       }
-   })
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+
+    if (boxTop < triggerBottom) {
+        foot.classList.add("show");
+      } else {
+        foot.classList.remove("show");;
+      }
+
+  });
 }
-
 
 //animate on scroll library or Jquery as an alternative to this
